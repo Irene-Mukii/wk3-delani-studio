27a7c98f38089ca7b8afa04debd690d6ed105264
renniequin@gmail.com <renniequin@gmail.com>
2021-03-01 21:50:54 +0300

    Adds the hover effect for the rest of the images.**Texts still not over image

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,48 @@ $(document).ready(function(){
         $("#product-showing").toggle();
         $("#product-hidden").toggle();
     })
-    $(document).ready(function(){
-        $("#img8").hover(function(){
-            $(this).find('#title').fadeIn(300);
-        }, function() {
-            $(this).find('#title').fadeOut(100);
-        });
-      });
+    $("#img1").hover(function(){
+        $(this).find('#title1').fadeIn(300);
+    }, function() {
+        $(this).find('#title1').fadeOut(100);
+    });
+    $("#img2").hover(function(){
+        $(this).find('#title2').fadeIn(300);
+    }, function() {
+        $(this).find('#title2').fadeOut(100);
+    });
+    $("#img3").hover(function(){
+        $(this).find('#title3').fadeIn(300);
+    }, function() {
+        $(this).find('#title3').fadeOut(100);
+    });
+    $("#img4").hover(function(){
+        $(this).find('#title4').fadeIn(300);
+    }, function() {
+        $(this).find('#title4').fadeOut(100);
+    });
+    $("#img5").hover(function(){
+        $(this).find('#title5').fadeIn(300);
+    }, function() {
+        $(this).find('#title5').fadeOut(100);
+    });
+    $("#img6").hover(function(){
+        $(this).find('#title6').fadeIn(300);
+    }, function() {
+        $(this).find('#title6').fadeOut(100);
+    });
+    $("#img7").hover(function(){
+        $(this).find('#title7').fadeIn(300);
+    }, function() {
+        $(this).find('#title7').fadeOut(100);
+    });
+    $("#img8").hover(function(){
+        $(this).find('#title8').fadeIn(300);
+    }, function() {
+        $(this).find('#title8').fadeOut(100);
+    });
+
+    
 })
 function submission(event){
     
